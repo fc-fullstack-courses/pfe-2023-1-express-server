70f7f2648514827688dad5aec836c221cbb3d16d
Kyrylo <fresh.js.ftd.@gmail.com>
2024-02-15 19:59:18 +0200

    Add basic middleware example

--- server.js
+++ server.js
@@ -6,12 +6,38 @@ const users = [{ id: 1 }, { id: 2 }];
 const app = express();
 
 // app містить функції які відповідають всім методам HTTP запиту
+// ендпоінт (шлях, ручка) - певний шлях на сервері (/users) з певним HTTP методом
 app.get('/users', (request, response) => {
   console.log('users requested');
 
   // response.end(JSON.stringify(users));
   response.send(users);
 });
+
+app.get('/test', (req, res, next) => {
+  console.log('first callback');
+
+  req.abracadabra = 'magic trick';
+
+  // функція яка сигналізує що можна переходити 
+  // на наступний обробник
+  next();
+}, (req, res, next) => {
+  console.log('second callback');
+  console.log(req.abracadabra);
+
+  if(Math.random() > 0.5) {
+    next();
+  } else {
+    res.send('ERROR happened');
+  }
+
+
+},(req, res) => {
+  console.log('third callback');
+  res.send('all done');
+});
+
 // app.get('/orders', (request, response) => {
 //   console.log('orders requested');
 
